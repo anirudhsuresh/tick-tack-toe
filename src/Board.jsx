2f578c01094a08753dd4ee5274947d6e5094e24e
anirudhsuresh <aniramch@ucdavis.edu>
2023-10-27 01:44:05 -0400

    winner logics

--- src/Board.jsx
+++ src/Board.jsx
@@ -9,9 +9,18 @@ function Board() {
     const [squares, setSquares] = useState(Array(9).fill(null))
 
 
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+        status = "Winner: " + winner;
+    } else {
+        status = "Next player: " + (xIsNext ? "X" : "O");
+    }
+
+
     function handleClick(i) {
 
-        if (squares[i]) {
+        if (squares[i] || calculateWinner(squares)) {
             return;
         }
 
@@ -30,6 +39,10 @@ function Board() {
 
     return (
         <>
+
+
+
+            <div className="status">{status}</div>
             <div className="board-row">
                 <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
                 <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -47,6 +60,27 @@ function Board() {
             </div>
         </>
     )
+
+
+    function calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
 }
 
 export default Board
