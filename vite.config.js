63b8fe084c4e6b29bccf1b18924a8a0bacecc394
anirudhsuresh <aniramch@ucdavis.edu>
2023-10-27 02:26:06 -0400

    fix config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "tick-tack-toe",
+  base: "/tick-tack-toe/",
   plugins: [react()],
 })
